ca044dad2b928cd0b1213adb556daaa607d0dd60
Andre Steenveld <andre.steenveld@gmail.com>
2017-03-25 22:17:45 +0100

    Installed and configured nunjucks

--- gulp/html.js
+++ gulp/html.js
@@ -1,11 +1,20 @@
 'use strict';
 
 var gulp = require('gulp'),
-  connect = require('gulp-connect');
+  connect = require('gulp-connect'),
+  nunjucks = require( "gulp-nunjucks-md" );
 
 // HTML livereload.
 gulp.task('html', function () {
   gulp.src(global.paths.html)
+    .pipe( 
+      nunjucks({
+        path: [ global.paths.src ],
+        data: {
+          environment: "development"
+        }
+      })
+    )
     .pipe(gulp.dest(global.paths.build))
     .pipe(connect.reload());
 });
