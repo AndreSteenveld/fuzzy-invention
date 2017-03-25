65841a3defa1518e0d32be9430709580c962d747
bas <bas>
2017-03-25 18:49:46 +0100

    installed neat, bourbon and normalize sass

--- gulp/sass.js
+++ gulp/sass.js
@@ -1,19 +1,35 @@
 'use strict';
 
-var gulp = require('gulp'),
+var 
+  path = require( "path" ),
+  gulp = require('gulp'),
   autoprefixer = require('gulp-autoprefixer'),
   concat = require('gulp-concat'),
   connect = require('gulp-connect'),
   sass = require('gulp-sass'),
   sourcemaps = require('gulp-sourcemaps'),
   
-  bourbon = require( "bourbon" );
+  bourbon = require( "bourbon" ),
+  neat    = require( "bourbon-neat" ),
+  include_media = require( "include-media" );
 
+function resolve_package_lib_dir( package_json_path ){
+
+  var package_path = path.dirname( require.resolve( package_json_path ) ),
+     package_json = require( package_json_path );
+	 
+  return [ path.join( package_path, package_json.directories.lib ) ];
+	
+}
+  
 var sassOptions = {
   errLogToConsole: true,
   outputStyle: 'expanded',
   includePaths: [ ]
 	.concat( bourbon.includePaths )
+	.concat( neat.includePaths )
+	.concat([ include_media.includePath ])
+	.concat( resolve_package_lib_dir( "normalize-scss/package.json" ) )
 };
 
 // Compile SASS with sourcemaps + livereload.
